c4ccf84098826b6bb78362349ff480edb1569c1d
JayLan <jaylan@bu.edu>
2018-06-28 22:38:52 -0400

    Implemented browser detection using bowser.js library.

--- js/scripts_main.js
+++ js/scripts_main.js
@@ -1,9 +1,11 @@
-let cssBool = CSS.supports('flex', true);
-let browName = navigator.appName;
-let browVers = navigator.appVersion;
+
+//let cssBool = CSS.supports('flex', true);
 
 setTimeout(() => {
-    if (cssBool == false && browName != 'Chrome' && browVers <= 67.0 ) {
-        alert("\nHi, your browser may not be displaying this page correctly!\n\nThis site uses the latest responsive layout standards.\n\nPlease update your browser. I recommend the most recent version of Google Chrome at:\n\n www.google.com/chrome/\n");
+    //alert(bowser.name);
+    if (bowser.name != "Chrome") {
+        alert(`\nHi, your browser, ${bowser.name} v.${bowser.version}, may not be displaying this page correctly!\n\nThis site uses the latest responsive layout standards.\n\nPlease update your browser. I recommend the most recent version of Google Chrome at:\n\n www.google.com/chrome/\n`);
+    }else{
+        console.log("Yay, you are using Chrome!");
     }
-}, 1000);
+}, 750);
